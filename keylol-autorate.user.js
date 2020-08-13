0a33c020831022ac1a06af6e67bc8f1e10100a43
matthewliu <ohperhaps.lyc@gmail.com>
2020-08-13 18:04:49 +0800

    Fix situation when scores greater than the sum of all remains

--- keylol-autorate.user.js
+++ keylol-autorate.user.js
@@ -3,7 +3,7 @@
 // @namespace    Keylol
 // @include      https://keylol.com/*
 // @require      https://code.jquery.com/jquery-3.5.1.min.js#sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=
-// @version      1.0.4
+// @version      1.0.5
 // @icon         https://raw.githubusercontent.com/ohperhaps/Keylol-Autorate/master/img/konoha.png
 // @downloadURL	 https://github.com/ohperhaps/Keylol-Autorate/raw/master/keylol-autorate.user.js
 // @updateURL	 https://github.com/ohperhaps/Keylol-Autorate/raw/master/keylol-autorate.user.js
@@ -25,9 +25,13 @@
 3.version 1.0.3 (2020-08-12) （感谢 @Zayne. @zjiang322）
    a.增加用户名显示
    b.增加无体力可加时的提示信息
+
 4.version 1.0.4 (2020-08-13) (感谢 @虫虫大作战 )
    a.修复代码逻辑错误造成的体力未加完的情况
    b.修改提示信息，增加控制台debug信息
+
+5.version 1.0.5 (2020-08-13) (感谢 @丨OTZ丨_sqkUw )
+   a.修复当前可加体力多于需要加体力时的崩溃问题
  */
 (function() {
     'use strict';
@@ -64,13 +68,12 @@
     }
     async function getUserScore() {
         let threads = await xhrAsync(`forum.php?mod=guide&view=newthread`).then((res) => {
-            let threads = new Array()
+            let threads = []
             $("div.bm_c", res.response).find("tbody").each(function () { threads.push($(this).attr("id").split("_").pop()) })
             return threads })
-        body:
         for (let thread of threads) {
             let posts = await xhrAsync(`t${thread}-1-1`).then((res) => {
-                let posts = new Array()
+                let posts = []
                 $("#postlist > div[id^=post_]", res.response).each(function () { posts.push($(this).attr("id").split("_").pop()) })
                 return posts
             })
@@ -105,7 +108,7 @@
     }
     function getCollections() {
         return xhrAsync(`plugin.php?id=keylol_favorite_notification:favorite_enhance&formhash=${formHash}`).then((res) => {
-            let collections = new Array()
+            let collections = []
             $("#delform", res.response).find("tr").each(function () {
                 let quote = formatQuote($("span.favorite_quote.xi1", this).text())
                 if (quote) {
@@ -123,14 +126,15 @@
     async function calcScores() {
         return Promise.all([getCollections(), getUserCredit(selfUid)]).then((results) => {
             let total = results[1].total
-            while(total > 0 ) {
-                if (results[0].length === 0) { break }
+            let calcFlag = results[0].length > 0
+            while(calcFlag) {
                 for(let item of results[0]) {
-                    if (total < 1) { break } else {
-                        if (item.score >= item.remain) { continue }
-                        else {item.score++; total--}
+                    if (total < 1) { calcFlag = false; break }
+                    else {
+                        if (item.score < item.remain) { item.score++; total-- }
                     }
                 }
+                if (results[0].every(item => item.score === item.remain)) { calcFlag = false }
             }
             results[0].forEach(function (item) {item.step = results[1].step})
             return [results[0], results[1].total]
@@ -138,7 +142,7 @@
     }
     function getUserReplys(uid, page=1) {
         return xhrAsync(`home.php?mod=space&uid=${uid}&do=thread&view=me&from=space&type=reply&order=dateline&page=${page}`).then((res) => {
-            let replys = new Array()
+            let replys = []
             $("#delform", res.response).find("td.xg1").each(function () {
                 let urlParams = new URLSearchParams($(this).find("a").attr("href"))
                 replys.push({tid: urlParams.get("ptid"),
@@ -152,8 +156,6 @@
         let quote_num = quote.match(/\d+/g)
         if (/^\d+\/\d+$/.test(quote) && parseInt(quote_num[0]) < parseInt(quote_num[1])) {
             return [(parseInt(quote_num[0]) + parseInt(addend)).toString() + '/' + quote_num[1].toString(), (parseInt(quote_num[1]) - parseInt(quote_num[0]) - parseInt(addend))]
-        } else {
-            return
         }
     }
     function updateQuote(favid, quote) {
@@ -189,6 +191,7 @@
         let message = []
         let itemScores = await calcScores()
         console.log(itemScores)
+        if (itemScores[0].length === 0) { message.push('未找到正确格式的收藏帖子！\n') }
         body:
         for (let item of itemScores[0]) {
             if (itemScores[1] === 0) { message.push('当前无剩余体力！请稍后再尝试！\n'); break }
